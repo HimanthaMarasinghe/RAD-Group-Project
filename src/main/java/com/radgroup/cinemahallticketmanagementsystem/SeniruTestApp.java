85b304cfb294fc263c50c3b1fbbde0d3144c01b0
SeniruR <seniruranasinghe@gmail.com>
2024-11-04 21:06:03 +0530

    Movie Details page created

---
+++ src/main/java/com/radgroup/cinemahallticketmanagementsystem/SeniruTestApp.java
@@ -0,0 +1,26 @@
+package com.radgroup.cinemahallticketmanagementsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SeniruTestApp extends Application {
+
+    public static String userName;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MovieDetails.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Movie Details");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
