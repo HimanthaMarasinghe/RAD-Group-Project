be99f7fc9ce1e60dd722e73729666a8631b4720c
SeniruR <seniruranasinghe@gmail.com>
2024-11-04 19:40:21 +0530

    Home page created and welcome note

--- src/main/java/com/radgroup/cinemahallticketmanagementsystem/controllers/Home.java
+++ src/main/java/com/radgroup/cinemahallticketmanagementsystem/controllers/Home.java
@@ -2,15 +2,16 @@ package com.radgroup.cinemahallticketmanagementsystem.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 public class Home implements DataReceiver{
     @FXML
-    private Label welcome;
+    private Text welcome;
 
     @Override
     public void receiveData(Object data) {
         if (data instanceof String) {
-            welcome.setText((String) data); // Set the username on the label
+            welcome.setText("Welcome "+(String) data); // Set the username on the label
         }
     }
 }
